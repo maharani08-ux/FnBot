d6c1b367080cf24a70df0b23b15158c8100510d8
maharani08-ux <maharanialfina08@gmail.com>
2026-06-24 18:02:36 +0700

    fix: pakai jsonbin

--- api.py
+++ api.py
@@ -1,14 +1,21 @@
 from flask import Flask, jsonify
-import json
+import requests
 import os
 
 app = Flask(__name__)
 
+JSONBIN_ID = os.getenv("JSONBIN_ID")
+JSONBIN_KEY = os.getenv("JSONBIN_KEY")
+
 @app.route("/music")
 def get_music():
-    with open("music.json", "r") as f:
-        data = json.load(f)
-    return jsonify(data)
+    url = f"https://api.jsonbin.io/v3/b/{JSONBIN_ID}/latest"
+    headers = {
+        "X-Master-Key": JSONBIN_KEY
+    }
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    return jsonify(data["record"])
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
